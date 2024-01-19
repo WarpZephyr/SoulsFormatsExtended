d91c6061b76e059ab1e6600661b35c548c78b34f
WarpZephyr <107563368+WarpZephyr@users.noreply.github.com>
2024-01-19 04:28:35 -0500

    Renamed SoulsFormats.AC3.BND0 to SoulsFormats.BND2, improved BND2 class

---
+++ SoulsFormats/Formats/BND2.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SoulsFormats
+{
+    /// <summary>
+    /// A Binder2 archive.
+    /// <para>Appears in Metal Wolf Chaos and the following Armored Core Games: Formula Front PSP, Nine Breaker, Last Raven.</para>
+    /// <para>Build settings for a file of this format were found in Armored Core Formula Front PSP which mentioned an app named Binder2.</para>
+    /// </summary>
+    public class BND2 : SoulsFile<BND2>
+    {
+        /// <summary>
+        /// An enum for the different supported file path modes.
+        /// </summary>
+        public enum FilePathModeEnum : byte
+        {
+            /// <summary>
+            /// Files in this BND have no name, only an ID.
+            /// </summary>
+            Nameless = 0,
+
+            /// <summary>
+            /// Files in this BND have names immediately after the file entries with no offset to the names array.
+            /// </summary>
+            NamesNoOffset = 1,
+
+            /// <summary>
+            /// Files in this BND have paths immediately after the file entries with no offset to the paths array. 
+            /// </summary>
+            Paths = 2,
+
+            /// <summary>
+            /// Files in this BND have names with an offset to the names array, the first being a base directory.
+            /// </summary>
+            NamesOffset = 3
+        }
+
+        /// <summary>
+        /// File version.
+        /// </summary>
+        public int FileVersion { get; set; }
+
+        /// <summary>
+        /// The alignment of each file.
+        /// <para>The bigger the aligment, the more empty bytes are added as padding. This increases the size of the archive.</para>
+        /// </summary>
+        public ushort AlignmentSize { get; set; }
+
+        /// <summary>
+        /// The file path mode determining how paths are handled.
+        /// </summary>
+        public FilePathModeEnum FilePathMode { get; set; }
+
+        /// <summary>
+        /// Unknown; Was found set to 1 on files extracted from memory.
+        /// Usually set to 0.
+        /// </summary>
+        public byte Unk1B { get; set; }
+
+        /// <summary>
+        /// The base directory of all files.
+        /// <para>Only used in <see cref="FilePathModeEnum.NamesOffset"/>.</para>
+        /// </summary>
+        public string BaseDirectory { get; set; }
+
+        /// <summary>
+        /// Files in this <see cref="BND2"/>.
+        /// </summary>
+        public List<File> Files { get; set; }
+
+        /// <summary>
+        /// Creates an empty <see cref="BND2"/>.
+        /// </summary>
+        public BND2()
+        {
+            FileVersion = 211;
+            AlignmentSize = 2048;
+            FilePathMode = FilePathModeEnum.NamesNoOffset;
+            Unk1B = 0;
+            BaseDirectory = string.Empty;
+            Files = new List<File>();
+        }
+
+        /// <summary>
+        /// Creates an empty <see cref="BND2"/> with the specified <see cref="FilePathMode"/>.
+        /// </summary>
+        public BND2(FilePathModeEnum filePathMode)
+        {
+            FileVersion = 211;
+            AlignmentSize = 2048;
+            FilePathMode = filePathMode;
+            Unk1B = 0;
+            BaseDirectory = string.Empty;
+            Files = new List<File>();
+        }
+
+        /// <summary>
+        /// Checks whether the data appears to be a file of this format.
+        /// </summary>
+        protected override bool Is(BinaryReaderEx br)
+        {
+            if (br.Length < 32)
+                return false;
+
+            string magic = br.ReadASCII(4);
+            uint unk04 = br.ReadUInt32();
+            int fileVersion = br.ReadInt32();
+            br.Position += 8; // File Size, File Num
+            int namesOffset = br.ReadInt32();
+            ushort alignmentSize = br.ReadUInt16();
+            byte filePathMode = br.ReadByte();
+            byte unk1B = br.ReadByte();
+            uint unk1C = br.ReadUInt32();
+
+            // All file path modes except for one have namesOffset set to 0.
+            bool validNamesOffset;
+            switch (filePathMode)
+            {
+                case 0:
+                case 1:
+                case 2:
+                    validNamesOffset = namesOffset <= br.Length && namesOffset == 0;
+                    break;
+                case 3:
+                    validNamesOffset = namesOffset <= br.Length;
+                    break;
+                default:
+                    // File path mode was invalid
+                    return false;
+            }
+
+            bool validMagic = magic == "BND\0";
+            bool expectedUnk04 = unk04 == 0xFFFF;
+            bool expectedAligmentSize = alignmentSize % 2 == 0;
+            bool expectedFileVersion = fileVersion >= 202 && fileVersion <= 211;
+            bool expectedUnk1B = unk1B == 0 || unk1B == 1;
+            bool expectedUnk1C = unk1C == 0;
+            return validMagic && expectedUnk04 && expectedFileVersion && validNamesOffset && expectedAligmentSize && expectedUnk1B && expectedUnk1C;
+        }
+
+        /// <summary>
+        /// Deserializes file data from a stream.
+        /// </summary>
+        protected override void Read(BinaryReaderEx br)
+        {
+            br.BigEndian = false;
+
+            br.AssertASCII("BND\0");
+            br.AssertUInt32(0xFFFF);
+            FileVersion = br.AssertInt32(202, 211); // Versions between 202 and 211 not seen.
+            br.Position += 4; // File Size
+            int fileCount = br.ReadInt32();
+            int namesOffset = br.ReadInt32();
+            AlignmentSize = br.ReadUInt16();
+            FilePathMode = br.ReadEnum8<FilePathModeEnum>();
+            Unk1B = br.AssertByte(0,1);
+            br.AssertUInt32(0);
+
+            if (FilePathMode == FilePathModeEnum.NamesOffset)
+            {
+                br.StepIn(namesOffset);
+                BaseDirectory = br.ReadShiftJIS();
+                br.StepOut();
+            }
+
+            Files = new List<File>(fileCount);
+            for (int i = 0; i < fileCount; i++)
+            {
+                Files.Add(new File(br, FilePathMode));
+            }
+        }
+
+        /// <summary>
+        /// Serializes file data to a stream.
+        /// </summary>
+        protected override void Write(BinaryWriterEx bw)
+        {
+            bw.BigEndian = false;
+
+            bw.WriteASCII("BND\0");
+            bw.WriteUInt32(0xFFFF);
+            bw.WriteInt32(FileVersion);
+            bw.ReserveInt32("fileSize");
+            bw.WriteInt32(Files.Count);
+            bw.ReserveInt32("namesOffset");
+            bw.WriteUInt16(AlignmentSize);
+            bw.WriteByte((byte)FilePathMode);
+            bw.WriteByte(Unk1B);
+            bw.WriteUInt32(0);
+
+            for (int i = 0; i < Files.Count; i++)
+            {
+                Files[i].Write(bw, FilePathMode, i);
+            }
+
+            switch (FilePathMode)
+            {
+                case FilePathModeEnum.Nameless:
+                    bw.FillInt32("namesOffset", 0);
+                    break;
+                case FilePathModeEnum.NamesNoOffset:
+                    bw.FillInt32("namesOffset", 0);
+                    for (int i = 0; i < Files.Count; i++)
+                    {
+                        bw.FillInt32($"nameOffset_{i}", (int)bw.Position);
+                        bw.WriteShiftJIS(Files[i].Name, true);
+                    }
+                    break;
+                case FilePathModeEnum.Paths:
+                    bw.FillInt32("namesOffset", 0);
+                    for (int i = 0; i < Files.Count; i++)
+                    {
+                        bw.FillInt32($"nameOffset_{i}", (int)bw.Position);
+
+                        string name = Files[i].Name;
+                        if (!Path.IsPathRooted(name))
+                        {
+                            name = Path.Combine("K:\\", name);
+                        }
+                        bw.WriteShiftJIS(name, true);
+                    }
+                    break;
+                case FilePathModeEnum.NamesOffset:
+                    bw.FillInt32("namesOffset", (int)bw.Position);
+                    bw.WriteShiftJIS(BaseDirectory, true);
+                    for (int i = 0; i < Files.Count; i++)
+                    {
+                        bw.FillInt32($"nameOffset_{i}", (int)bw.Position);
+                        bw.WriteShiftJIS(Files[i].Name, true);
+                    }
+                    break;
+                default:
+                    throw new NotSupportedException($"{nameof(FilePathMode)} {FilePathMode} is not supported.");
+            }
+
+            for (int i = 0; i < Files.Count; i++)
+            {
+                bw.Pad(AlignmentSize);
+                bw.FillInt32($"fileOffset_{i}", (int)bw.Position);
+                bw.WriteBytes(Files[i].Bytes);
+            }
+
+            bw.FillInt32($"fileSize", (int)bw.Length);
+        }
+
+        /// <summary>
+        /// A file in a BND0.
+        /// </summary>
+        public class File
+        {
+            /// <summary>
+            /// ID of this file.
+            /// </summary>
+            public int ID { get; set; }
+
+            /// <summary>
+            /// The name of this file.
+            /// <para>Will be set to ID if name does not exist.</para>
+            /// <para>Will be a path with a drive letter if <see cref="FilePathModeEnum.Paths"/> is set.</para>
+            /// <para>Will need <see cref="BaseDirectory"/> added as the base directory if <see cref="FilePathModeEnum.NamesOffset"/> is set.</para>
+            /// </summary>
+            public string Name { get; set; }
+
+            /// <summary>
+            /// The raw data of this file.
+            /// </summary>
+            public byte[] Bytes { get; set; }
+
+            /// <summary>
+            /// Creates a File.
+            /// </summary>
+            public File()
+            {
+                ID = -1;
+                Name = string.Empty;
+                Bytes = null;
+            }
+
+            /// <summary>
+            /// Creates a File with an ID.
+            /// </summary>
+            public File(int id)
+            {
+                ID = id;
+                Name = string.Empty;
+                Bytes = null;
+            }
+
+            /// <summary>
+            /// Creates a File with a name.
+            /// </summary>
+            public File(string name)
+            {
+                ID = -1;
+                Name = name;
+                Bytes = null;
+            }
+
+            /// <summary>
+            /// Creates a File with bytes.
+            /// </summary>
+            public File(byte[] bytes)
+            {
+                ID = -1;
+                Name = string.Empty;
+                Bytes = bytes;
+            }
+
+            /// <summary>
+            /// Creates a File with an ID and name.
+            /// </summary>
+            public File(int id, string name)
+            {
+                ID = id;
+                Name = name;
+                Bytes = null;
+            }
+
+            /// <summary>
+            /// Creates a File with an ID and bytes.
+            /// </summary>
+            public File(int id, byte[] bytes)
+            {
+                ID = id;
+                Bytes = bytes;
+            }
+
+            /// <summary>
+            /// Creates a File with an id, name, and bytes.
+            /// </summary>
+            public File(int id, string name, byte[] bytes)
+            {
+                ID = id;
+                Name = name;
+                Bytes = bytes;
+            }
+
+            internal File(BinaryReaderEx br, FilePathModeEnum filePathMode)
+            {
+                ID = br.ReadInt32();
+                int offset = br.ReadInt32();
+                int size = br.ReadInt32();
+                int nameOffset = br.ReadInt32();
+
+                switch (filePathMode)
+                {
+                    case FilePathModeEnum.Nameless:
+                        Name = ID.ToString();
+                        break;
+                    case FilePathModeEnum.NamesNoOffset:
+                    case FilePathModeEnum.Paths:
+                    case FilePathModeEnum.NamesOffset:
+                        Name = br.GetShiftJIS(nameOffset);
+                        break;
+                    default:
+                        throw new NotSupportedException($"{nameof(filePathMode)} {filePathMode} is not supported.");
+                }
+
+                Bytes = br.GetBytes(offset, size);
+            }
+
+            /// <summary>
+            /// Serializes file data to a stream.
+            /// </summary>
+            internal void Write(BinaryWriterEx bw, FilePathModeEnum filePathMode, int index)
+            {
+                bw.WriteInt32(ID);
+                bw.ReserveInt32($"fileOffset_{index}");
+                bw.WriteInt32(Bytes.Length);
+
+                if (filePathMode == FilePathModeEnum.Nameless)
+                {
+                    bw.WriteInt32(0);
+                }
+                else
+                {
+                    bw.ReserveInt32($"nameOffset_{index}");
+                }
+            }
+        }
+    }
+}
